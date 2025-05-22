d6c7e3681c791b28c77753e1fc9a5935f7787163
rwam1 <rwam1.nikhil.ub@gmail.com>
2025-05-22 12:38:22 +0530

    add

--- server.js
+++ server.js
@@ -28,6 +28,27 @@ app.post('/adduser',(req,res)=>{
     res.redirect('/');
 
 })
+app.get('/edituser/:id',(req,res)=>{
+    let id = req.params.id
+    let user = users.find((it)=> it.id ==id)
+    console.log(user);
+    
+ res.render('EditUser',{user}) 
+
+})
+app.post('/edituser/:id',(req,res)=>{
+    let {name ,email} =req.body
+    console.log(name,email);
+    console.log()
+
+    let user = users.find((it)=>it.id ==req.params.id)
+     user.name =name
+     user.email =email
+    console.log(users);
+    res.redirect('/')
+})
+
+
 
 
 
